eadfd56d6cea0c90ec1930ecbcb5a97fa5811d62
Cameron Legleiter <camlegleiter@gmail.com>
2017-05-25 07:07:49 -0500

    Adding Blink library and dependencies

---
+++ src/blink.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  getInitialState() {
+    return {
+      isVisible: true,
+    };
+  }
+
+  componentDidMount() {
+    this._animate();
+  }
+
+  componentDidUpdate() {
+    this._animate();
+  }
+
+  componentWillUnmount() {
+    clearInterval(this._blinkTimer);
+  }
+
+  render() {
+    const { className, text } = this.props;
+    const style = {
+      visibility: this.state.isVisible ? 'visible' : 'hidden',
+    };
+
+    return (
+      <div className={`ui-blink ${className}`}>
+        <span ref="text" style={style}>{children}</span>
+      </div>
+    );
+  }
+
+  _animate() {
+    clearInterval(this._blinkTimer);
+    const { rate } = this.props;
+
+    this._blinkTimer = setInterval(() => {
+      this.setState({
+        isVisible: !this.state.isVisible,
+      });
+    }, rate);
+  }
+}
+
+Blink.propTypes = {
+  rate: PropTypes.number,
+  className: PropTypes.string,
+  children: PropTypes.string,
+};
+
+Blink.defaultProps = {
+  rate: 1000,
+  className: '',
+  children: '',
+}
+
+export default Blink;
